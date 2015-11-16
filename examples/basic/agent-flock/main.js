2c370b5d72977a4d3acea5d7f9aab260ed59ecd4
Brannon Dorsey <brannon@brannondorsey.com>
2015-11-16 12:50:46 -0600

    Add agent-flock example

---
+++ examples/basic/agent-flock/main.js
@@ -0,0 +1,104 @@
+var canvas = document.createElement('canvas');
+canvas.id = 'canvas';
+document.body.appendChild(canvas);
+
+document.body.className = "radial-grey";
+
+var ctx        = canvas.getContext('2d');
+var mouseInput = new BB.MouseInput(canvas);
+
+var WIDTH, HEIGHT, agents = [];
+var seperateForce, alignForce, cohesionForce;
+var seperateMult, alignMult, cohesionMult;
+
+function setup() {
+    
+    window.onresize = function() {
+        WIDTH = canvas.width = window.innerWidth;
+        HEIGHT = canvas.height = window.innerHeight;
+    }
+    
+    window.onresize();
+
+    var amount = 50; //gui -name amount
+    seperateForce = 0.2; //gui -name seperateMaxForce -min 0.1 -max 0.7 -step 0.1
+    alignForce = 0.2; //gui -name alignMaxForce -min 0.1 -max 0.7 -step 0.1
+    cohesionForce = 0.2; //gui -name cohesionMaxForce -min 0.1 -max 0.7 -step 0.1
+    seperateMult = 1; //gui -name seperateMult -min 0.1 -max 1.0 -step 0.1
+    alignMult = 1; //gui -name alignMult -min 0.0 -max 1.0 -step 0.1
+    cohesionMult = 1; //gui -name cohesionMult -min 0.0 -max 1.0 -step 0.1
+
+    for (var i = 0; i < amount; i++) {
+        
+        var agent = new BB.Agent2D({
+            maxSpeed: 6, //gui -name maxSpeed -min 5 -max 10
+            position: new BB.Vector2( Math.random() * WIDTH, Math.random() * HEIGHT ),
+            velocity: new BB.Vector2( BB.MathUtils.randomFloat(-1,1), BB.MathUtils.randomFloat(-5,5) ),
+            radius: 50
+        });
+
+        agents.push(agent);
+    };
+}
+
+function update() {
+    
+    requestAnimationFrame(update);
+
+    mouseInput.update();
+
+    var mouse = new BB.Vector2(mouseInput.x, mouseInput.y);
+
+    for (var i = 0; i < agents.length; i++) {
+
+        agents[i].seperate(agents, seperateForce, 30, seperateMult);
+        agents[i].align(agents, alignForce, null, alignMult);
+        agents[i].cohesion(agents, cohesionForce, null, cohesionMult);
+        agents[i].update();
+
+        // atari style boarders
+        if (agents[i].position.x < -agents[i].radius)  
+            agents[i].position.x = WIDTH + agents[i].radius;
+
+        if (agents[i].position.y < -agents[i].radius)  
+            agents[i].position.y = HEIGHT + agents[i].radius;
+
+        if (agents[i].position.x > WIDTH + agents[i].radius) 
+            agents[i].position.x = -agents[i].radius;
+
+        if (agents[i].position.y > HEIGHT + agents[i].radius) 
+            agents[i].position.y = -agents[i].radius;
+
+    };
+
+    draw();
+}
+
+function draw() {
+    
+    ctx.clearRect(0,0,WIDTH,HEIGHT);
+
+    var width = 30;
+    var height = 50;
+
+    for (var i = 0; i < agents.length; i++) {
+       
+        ctx.fillStyle = "#cc3399";
+        ctx.save();
+        ctx.translate(agents[i].position.x, agents[i].position.y);
+    
+        // add 90 degrees to rotate triangle (which is drawn pointy side up)
+        ctx.rotate(agents[i].heading + BB.MathUtils.degToRad(90));
+        ctx.beginPath();
+        ctx.moveTo(-width/2, height/2);
+        ctx.lineTo(0, - height/2);
+        ctx.lineTo(width/2, height/2);
+        ctx.closePath();
+        ctx.fill();
+        ctx.restore();
+    };
+
+}
+
+setup();
+update();
--- examples/basic/agent-seek/main.js
+++ examples/basic/agent-seek/main.js
@@ -1,12 +1,14 @@
-var canvas     = document.createElement('canvas');
+var canvas = document.createElement('canvas');
 canvas.id = 'canvas';
 document.body.appendChild(canvas);
+
+document.body.style.backgroundColor = "#FFB6E6";
 
 var ctx        = canvas.getContext('2d');
 var gravity    = new BB.Vector2();
 var mouseInput = new BB.MouseInput(canvas);
 
-var WIDTH, HEIGHT, agents = [];
+var WIDTH, HEIGHT, agent;
 
 function setup() {
     
@@ -16,26 +18,14 @@ function setup() {
     }
     
     window.onresize();
-
-    var amount = 50; //gui -name amount
-    for (var i = 0; i < amount; i++) {
-        
-        var agent = new BB.Agent2D({
-            position: new BB.Vector2( Math.random()*WIDTH, Math.random()*HEIGHT ),
-            velocity: new BB.Vector2( BB.MathUtils.randomFloat(-1,1), BB.MathUtils.randomFloat(-5,5) ),
-            radius: BB.MathUtils.randomInt(15, 30),
-            elasticity: 0.1 //gui -name elasticity -min 0.0 -max 0.5 -steps 0.01
-        });
-
-        agent.collide({
-            top: 0,
-            right: canvas.width,
-            bottom: canvas.height,
-            left: 0
-        });
-
-        agents.push(agent);
-    };
+    
+    agent = new BB.Agent2D({
+        maxSpeed: 5, // setting the speed too high can cause the 'arrive' behavior not to work
+        position: new BB.Vector2( Math.random()*WIDTH, Math.random()*HEIGHT ),
+        velocity: new BB.Vector2( BB.MathUtils.randomFloat(-1,1), BB.MathUtils.randomFloat(-5,5) ),
+        radius: BB.MathUtils.randomInt(15, 30),
+        elasticity: 0.1 //gui -name elasticity -min 0.0 -max 0.5 -steps 0.01
+    });
 }
 
 function update() {
@@ -48,14 +38,9 @@ function update() {
 
     var mouse = new BB.Vector2(mouseInput.x, mouseInput.y);
 
-    for (var i = 0; i < agents.length; i++) {
-
-        agents[i].seek(mouse, 0.1, 100);
-
-        agents[i].applyForce(gravity);
-
-        agents[i].update();
-    };
+    agent.seek(mouse, 0.1, 150);
+    agent.applyForce(gravity);
+    agent.update();
 
     draw();
 }
@@ -66,24 +51,20 @@ function draw() {
 
     var width = 30;
     var height = 50;
-
-    for (var i = 0; i < agents.length; i++) {
        
-        ctx.fillStyle = "#cc3399";
-        ctx.save();
-        ctx.translate(agents[i].position.x, agents[i].position.y);
-    
-        // add 90 degrees to rotate triangle (which is drawn pointy side up)
-        ctx.rotate(agents[i].heading + BB.MathUtils.degToRad(90));
-        ctx.beginPath();
-        ctx.moveTo(-width/2, height/2);
-        ctx.lineTo(0, - height/2);
-        ctx.lineTo(width/2, height/2);
-        ctx.closePath();
-        ctx.fill();
-        ctx.restore();
-    };
+    ctx.fillStyle = "#cc3399";
+    ctx.save();
+    ctx.translate(agent.position.x, agent.position.y);
 
+    // add 90 degrees to rotate triangle (which is drawn pointy side up)
+    ctx.rotate(agent.heading + BB.MathUtils.degToRad(90));
+    ctx.beginPath();
+    ctx.moveTo(-width/2, height/2);
+    ctx.lineTo(0, - height/2);
+    ctx.lineTo(width/2, height/2);
+    ctx.closePath();
+    ctx.fill();
+    ctx.restore();
 }
 
 
