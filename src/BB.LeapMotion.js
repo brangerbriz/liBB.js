61088e0c2a5cf7244b8ae9291cf0f1413d9bcf39
Fabian Valencia <cfvalencia9277@gmail.com>
2016-03-10 09:13:27 -0500

    started fixes done with 1,2,3 and 5

--- src/BB.LeapMotion.js
+++ src/BB.LeapMotion.js
@@ -9,6 +9,7 @@
 define(['./BB'],
 function(BB){
 
+
     'use strict';
   /**
      * A module implementing LeapMotion Sensor.
@@ -16,31 +17,34 @@ function(BB){
      * __Note:__ The constructor makes throws an Error if the LeapMotion library is not already loaded.
      * If the LeapMotion library is not imported "missing LeapMotion library "  should appear in the console,
      * check LeapMotion docs or LiBB examples to see how to import library from html.
+     * Method  enables the LeapMotion module to start 
+     * obtaining the X,Y values from the sensor, these values must be called if needed.
+     * Method also allows to detect if a gesture has occured.
      * @class BB.LeapMotion
+     * @param {Canvas} canvas The created canvas that must be given to the LeapMotion module if canX and canvasY variable want to be used.
+     * @param {Boolean} getXY Boolean value to enable/disable access to the X,Y values from LeapMotion Sensor .
+     * @param {Boolean} getGestures Boolean value to enable/disable access to the getGestures from LeapMotion Sensor.
      * @constructor
      */
-   BB.LeapMotion = function(){ 
-       if(typeof Leap === 'undefined'){
-         throw new Error(' missing LeapMotion library ');
-       }
-     };
-   // create variables that can be accesed later on to be able to have the data
+  BB.LeapMotion = function(config){ 
+
+    // create variables that can be accesed later on to be able to have the data
    // canvasX, canvasY each will contain a numeric value that represent the position.
 
         /**
          * The pointers X position as given by the LeapMotion Sensor
-         * @property canvasX
+         * @property y
          * @type {Number}
          * @default 0
          */
-   BB.LeapMotion.prototype.canvasX = 0;
+   BB.LeapMotion.prototype.x = 0;
          /**
          * The pointers Y position as given by the LeapMotion Sensor
-         * @property canvasY
+         * @property y
          * @type {Number}
          * @default 0
          */
-   BB.LeapMotion.prototype.canvasY = 0;
+   BB.LeapMotion.prototype.y = 0;
          /**
          * Boolean value corresponding to the grab gesture detected by the LeapMotion sensor.
          * @property grab
@@ -106,86 +110,80 @@ function(BB){
    BB.LeapMotion.prototype.deviceStreaming = false;
   
 
-   //creating function to be called to access x,y in a fast and easy way
-   // function requires a canvas.
-   /**
-   * Method that enables the LeapMotion module to start 
-   * obtaining the X,Y values from the sensor, these values must be called if needed.
-   * Method also allows to detect if a gesture has occured.
-   * @method GetLeapData
-   * @param {Canvas} canvas The created canvas that must be given to the LeapMotion module if canX and canvasY variable want to be used.
-   * @param {Boolean} getXY Boolean value to enable/disable access to the X,Y values from LeapMotion Sensor .
-   * @param {Boolean} getGestures Boolean value to enable/disable access to the getGestures from LeapMotion Sensor.
-   */
-   // GetLeapData method accepts need 3 inputs
-   // 1 tha canvas created 
-   // 2 a boolean value to get X,Y values 
-   // 3 a boolean value to get gestures 
-   BB.LeapMotion.prototype.getLeapData= function(canvas , getXY , getGestures){
+
+       if(typeof Leap === 'undefined'){
+         throw new Error(' missing LeapMotion library ');
+       } 
    // using Leap. controller to create the connection to our sensor
         var controller = new Leap.Controller({enableGestures:true});
         var canvasGiven = false;
+
+        // gets data from config
+        var canvas = (config && typeof config.canvasConstructor === 'object') ? config.canvasConstructor : null;
+        var getXY = (config && typeof config.coordinatesEnabled === 'boolean') ? config.coordinatesEnabled : false;
+        var getGestures = (config && typeof config.gesturesEnabled === 'boolean') ? config.gesturesEnabled : false;
+
          if(canvas === null){
-          console.log("No canvas given therefor canvasX and canvasY are not available");
+          console.log("No canvas given therefor x and y are not available");
           canvasGiven = false;
         }else{canvasGiven = true;}
         // the controller.on method lets us se what the sensor is telling us on each frame
         // frames are sent 200 frames per second
         controller.on("frame",function(frame){
           // frame.pointables allows us to detect when a frame has a pointable.(hand,finger)
-                if(frame.pointables.length>0 && getXY && canvasGiven){
-                    var pointable = frame.pointables[0];
-                    // creates and interaction box it provides normalized coordinates for hands, fingers, and tools within this box.
-                    var interactionBox = frame.interactionBox;
-                    // provides the stabalized tip position
-                    var normalizedPosition = interactionBox.normalizePoint(pointable.stabilizedTipPosition, true);
-                    // Convert the normalized coordinates to span the canvas
-                    BB.LeapMotion.prototype.canvasX = canvas.width * normalizedPosition[0];
-                    BB.LeapMotion.prototype.canvasY = canvas.height * (1 - normalizedPosition[1]);
-                }
-                // make all vars false when no hand detected fromo the LeaMotion
-                if(frame.hands.length === 0){
-                    BB.LeapMotion.prototype.grab = false;
-                    BB.LeapMotion.prototype.pinch = false;
-                    BB.LeapMotion.prototype.circle = false;
-                    BB.LeapMotion.prototype.keytap = false;
-                    BB.LeapMotion.prototype.screenTap = false;
-                    BB.LeapMotion.prototype.swipe = false;
-                }
-                // detects hands and makes sure user wants to check for gestures.  
-                if(frame.hands.length > 0 && getGestures){
-                  var hand = frame.hands[0];
-                  var position = hand.palmPosition;
-                  // when a frame detects a hand and gestures are wanted it will give true when gesture grab     
-                  if(hand.grabStrength == 1){
-                    BB.LeapMotion.prototype.grab = true;
-                    BB.LeapMotion.prototype.pinch = false;
-                    BB.LeapMotion.prototype.circle = false;
-                    BB.LeapMotion.prototype.keytap = false;
-                    BB.LeapMotion.prototype.screenTap = false;
-                    BB.LeapMotion.prototype.swipe = false;
-                  }else{
-                    BB.LeapMotion.prototype.grab = false;
+                  if(frame.pointables.length>0 && getXY && canvasGiven){
+                      var pointable = frame.pointables[0];
+                      // creates and interaction box it provides normalized coordinates for hands, fingers, and tools within this box.
+                      var interactionBox = frame.interactionBox;
+                      // provides the stabalized tip position
+                      var normalizedPosition = interactionBox.normalizePoint(pointable.stabilizedTipPosition, true);
+                      // Convert the normalized coordinates to span the canvas
+                      BB.LeapMotion.prototype.x = canvas.width * normalizedPosition[0];
+                      BB.LeapMotion.prototype.y = canvas.height * (1 - normalizedPosition[1]);
                   }
-                  // when a frame detects a hand and gestures are wanted it will give true when gesture pinch     
-                  if(hand.pinchStrength == 1){
-                    BB.LeapMotion.prototype.grab = false;
-                    BB.LeapMotion.prototype.pinch = true;
-                    BB.LeapMotion.prototype.circle = false;
-                    BB.LeapMotion.prototype.keytap = false;
-                    BB.LeapMotion.prototype.screenTap = false;
-                    BB.LeapMotion.prototype.swipe = false;
-                 }else{
-                    BB.LeapMotion.prototype.pinch = false;
-                 }
-                } 
-            // when no gestures are being detected all the pre loaded gestures are falsed. (circle,keytap,screenTap,swipe)    
-            if(frame.valid && frame.gestures.length === 0){
-                    BB.LeapMotion.prototype.circle = false;
-                    BB.LeapMotion.prototype.keytap = false;
-                    BB.LeapMotion.prototype.screenTap = false;
-                    BB.LeapMotion.prototype.swipe = false;
-            } 
+                  // make all vars false when no hand detected fromo the LeaMotion
+                  if(frame.hands.length === 0){
+                      BB.LeapMotion.prototype.grab = false;
+                      BB.LeapMotion.prototype.pinch = false;
+                      BB.LeapMotion.prototype.circle = false;
+                      BB.LeapMotion.prototype.keytap = false;
+                      BB.LeapMotion.prototype.screenTap = false;
+                      BB.LeapMotion.prototype.swipe = false;
+                  }
+                  // detects hands and makes sure user wants to check for gestures.  
+                  if(frame.hands.length > 0 && getGestures){
+                    var hand = frame.hands[0];
+                    var position = hand.palmPosition;
+                    // when a frame detects a hand and gestures are wanted it will give true when gesture grab     
+                    if(hand.grabStrength == 1){
+                      BB.LeapMotion.prototype.grab = true;
+                      BB.LeapMotion.prototype.pinch = false;
+                      BB.LeapMotion.prototype.circle = false;
+                      BB.LeapMotion.prototype.keytap = false;
+                      BB.LeapMotion.prototype.screenTap = false;
+                      BB.LeapMotion.prototype.swipe = false;
+                    }else{
+                      BB.LeapMotion.prototype.grab = false;
+                    }
+                    // when a frame detects a hand and gestures are wanted it will give true when gesture pinch     
+                    if(hand.pinchStrength == 1){
+                      BB.LeapMotion.prototype.grab = false;
+                      BB.LeapMotion.prototype.pinch = true;
+                      BB.LeapMotion.prototype.circle = false;
+                      BB.LeapMotion.prototype.keytap = false;
+                      BB.LeapMotion.prototype.screenTap = false;
+                      BB.LeapMotion.prototype.swipe = false;
+                   }else{
+                      BB.LeapMotion.prototype.pinch = false;
+                   }
+                  } 
+              // when no gestures are being detected all the pre loaded gestures are falsed. (circle,keytap,screenTap,swipe)    
+              if(frame.valid && frame.gestures.length === 0){
+                      BB.LeapMotion.prototype.circle = false;
+                      BB.LeapMotion.prototype.keytap = false;
+                      BB.LeapMotion.prototype.screenTap = false;
+                      BB.LeapMotion.prototype.swipe = false;
+              } 
             // when a gesture is detected it will take each gesture and set to true the var that corresponds to the gesture 
             // making the gesture available to the user  
             if(frame.valid && frame.gestures.length > 0){              
@@ -225,6 +223,6 @@ function(BB){
       function onStopped(){
         BB.LeapMotion.prototype.deviceStreaming = false;
       }   
-   };
-   return BB.LeapMotion;
+  };
+  return BB.LeapMotion;
 });
