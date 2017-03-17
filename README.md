57c1f59b8e6a24e19ac1d4e77b29ff82ed69aebc
nbriz <nickbriz@gmail.com>
2017-03-17 16:06:37 -0500

    initial project structure

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # liBB
 
-...a new beginning 
+#### A JavaScript library/framework for creating interactive && generative apps + installations in/out of the browser
+visit [libb.brangerbriz.com](http://libb.brangerbriz.com/) for examples and documentation or see the [www](https://github.com/brangerbriz/liBB.js/blob/master/wwww) directory.
+
+----------
+
+## dev dependencies
+run `npm install` in the root directory to download the dev dependencies
+
+* **jshint** for linting the source code
+* **jshint-stylish** for prettier linting
+* **babel-cli** for transpiling
+* **babel-preset-es2015** transpiling preset from es2015
+* **browserify** for compiling the build
+* **uglify-js** for minifying the build
+* **yuidocjs** for auto-generated docs
+* **nodemon** for watch scripts
+
+## dev notes
+to build the library you can run `npm run build`, which will create two files ( in the build folder ) **BB.min.js** and **BB.js**. Running the build scripts also automatically runs `npm run lint` which will lint your code for errors, `npm run transpile` which will generate transpiled version of all the source code in **src/transpiled**, `npm run compile` which compiles the transpiled versions into **build/BB.js**, `npm run minify` which minifies the compiled library into **build/BB.min.js** and  `npm run docs` which builds the documentation page in **www/docs**.
+
+to build an ***un-***minified and ***un***-transpiled debug version of the library with source maps run `npm run build:debug`, this will produce **BB.debug.js** into the build folder as well as the www folder for use with examples and tests.
+
+There are also a couple of watch scripts which watch for changes in the **src/** directory: `npm run watch` which will run the entire build process described above, or `npm run watch:debug` which will only run the build:debug script.
---
+++ src/BB.js
@@ -0,0 +1,108 @@
+/*
+      +++++++++++++
+    +++++++++++++++++
+  +++++++++++++++++++++
+ +++++++ ---------- ++++       ____                                         ____       _
+++++++++|  ______  |+++++     |  _ \                                       |  _ \     (_)
+++++++__| |______| |+++++     | |_) |_ __ __ _ _ __   __ _  ___ _ __       | |_) |_ __ _ ____
++++++|  _________  |+++++     |  _ <| '__/ _` | '_ \ / _` |/ _ \ '__|      |  _ <| '__| |_  /
++++++| |_________| |+++++     | |_) | | | (_| | | | | (_| |  __/ |         | |_) | |  | |/ /
+ ++++|_____________|++++      |____/|_|  \__,_|_| |_|\__, |\___|_| _______ |____/|_|  |_/___|
+  +++++++++++++++++++++                              __ | |       |_______|
+    +++++++++++++++++                                \___/
+      +++++++++++++                                                   // libb.brangerbriz.com
+*/
+function BB() {
+
+	// ~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - .
+	// ~._.~'~._.~'~._.~'	 General Utils      '~._.~'~._.~'~._.~'~._.~'~._.~'
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - .
+	// ~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'
+
+	// this.ValidateParam 	= require('./utils/BB.ValidateParam');
+
+	// this.Detect			= require('./utils/BB.Detect');
+
+	// this.Vector2D 		= require('./utils/BB.Vector2D');
+
+	// this.Maths 			= require('./utils/BB.Maths');
+
+	// this.EventEmitter	= require('./utils/BB.EventEmitter');
+
+
+	// ~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - .
+	// ~._.~'~._.~'~._.~'	  Input Modules     '~._.~'~._.~'~._.~'~._.~'~._.~'
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - .
+	// ~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'
+
+
+	// this.MouseInput 	= require('./input/BB.MouseInput');
+	//
+	// this.LeapMotion		= require('./input/BB.LeapMotion');
+	//
+	// this.BaseMidiInput	= require('./input/BB.BaseMidiInput');
+	// this.MidiInputButton= require('./input/BB.MidiInputButton');
+	// this.MidiInputKey	= require('./input/BB.MidiInputKey');
+	// this.MidiInputKnob	= require('./input/BB.MidiInputKnob');
+	// this.MidiInputPad	= require('./input/BB.MidiInputPad');
+	// this.MidiInputSlider= require('./input/BB.MidiInputSlider');
+
+
+	// ~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - .
+	// ~._.~'~._.~'~._.~'	 Visual Modules     '~._.~'~._.~'~._.~'~._.~'~._.~'
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - .
+	// ~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'
+
+	// this.Color 			= require('./visual/BB.Color');
+	// this.Logo 			= require('./visual/BB.Logo');
+
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - |   2D   | - . - .
+
+	// this.Pointer 		= require('./visual/2d/BB.Pointer');
+	// this.BrushManager2D = require('./visual/2d/BB.BrushManager2D');
+	// this.BaseBrush2D 	= require('./visual/2d/BB.BaseBrush2D');
+	// this.ImageBrush2D 	= require('./visual/2d/BB.ImageBrush2D');
+	// this.LineBrush2D 	= require('./visual/2d/BB.LineBrush2D');
+	//
+	// this.Particle2D 	= require('./visual/2d/BB.Particle2D');
+	// this.Agent2D 		= require('./visual/2d/BB.Agent2D');
+	// this.FlowField2D 	= require('./visual/2d/BB.FlowField2D');
+
+
+	/* * * * * * * *
+	 *  2d_addons  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 * * * * * * * */
+
+	// this.SprayBrush 	= require('./addons/2d/BB.SprayBrush');
+	// this.LeafBrush 		= require('./addons/2d/BB.LeafBrush');
+	// this.RainbowBrush 	= require('./addons/2d/BB.RainbowBrush');
+	// this.StarBrush 		= require('./addons/2d/BB.StarBrush');
+	// this.WebBrush 		= require('./addons/2d/BB.WebBrush');
+
+
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - |   3D   | - . -
+
+
+	// ~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - .
+	// ~._.~'~._.~'~._.~'	  Audio Modules     '~._.~'~._.~'~._.~'~._.~'~._.~'
+	// - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - . - .
+	// ~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'~._.~'
+
+	// this.Audio 				= require('./audio/BB.Audio');
+	// this.AudioBase 			= require('./audio/BB.AudioBase');
+	// this.AudioBufferLoader 	= require('./audio/BB.AudioBufferLoader');
+	// this.AudioStream 		= require('./audio/BB.AudioStream');
+	// this.AudioAnalyser 		= require('./audio/BB.AudioAnalyser');
+	// this.AudioFX 			= require('./audio/BB.AudioFX');
+	// this.AFX 				= require('./audio/BB.AFX');
+	// this.AudioNoise 		= require('./audio/BB.AudioNoise');
+	// this.AudioTone 			= require('./audio/BB.AudioTone');
+	// this.AudioSampler 		= require('./audio/BB.AudioSampler');
+	// this.AudioSequencer 	= require('./audio/BB.AudioSequencer');
+
+}
+window.BB = new BB();
